958f80063600b1b18c2633287d737a6a62f2f8b3
Steve <srherzog@gmail.com>
2020-05-15 14:07:50 -0500

    Script update

--- splatter/__init__.py
+++ splatter/__init__.py
@@ -24,14 +24,13 @@
 
 from IPython.display import display, Javascript, HTML
 
-js_libs = ['https://otosense.analogcloudsandbox.io/static/js/tsne.js',
-           'https://otosense.analogcloudsandbox.io/static/js/splatter.js']
+js_libs = ['https://otosense-dev-ui.s3.amazonaws.com/static/js/tsne.js',
+           'https://otosense-dev-ui.s3.amazonaws.com/static/js/splatter.js']
 
 
-def splatter(data):
-    return Javascript("""
-    ((element) => {
+def splatter(nodes, options):
+    return Javascript(f"""
+    ((element) => {{
         console.log('HI!');
-        require(['splatter'], (splatter) => splatter(element.get(0), %s))
-    })(element)
-    """ % data, lib=js_libs)
+        require(['splatter'], (splatter) => splatter(element.get(0), {nodes}, {options}))
+    }})(element);""", lib=js_libs)
