09d236e72cb57f8688f43753fdaa3336c1cf4262
Steve Herzog <steve.herzog@analog.com>
2020-11-06 12:32:38 -0600

    Splatter OO refactoring

--- oui/index.ts
+++ oui/index.ts
@@ -1,5 +1,5 @@
 import { splatter } from './splatter';
-export { splatter } from './splatter';
+export { Splatter, splatter } from './splatter';
 export { default as SoundUtility } from './sound_utils';
 export * from './multi_time_vis';
 import { renderTimeChannel } from './multi_time_vis';
